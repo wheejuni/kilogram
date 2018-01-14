e862d730b8f1366661a2a32ed82f210ed22b8a31
seulgiwendy <wheejuni@gmail.com>
2018-01-14 13:46:19 +0900

    [view]fixed typo on kilogram/forms.py

--- kilogram/forms.py
+++ kilogram/forms.py
@@ -11,7 +11,7 @@ class CreateUserForm(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
 
     def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
+        user = super(CreateUserForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
