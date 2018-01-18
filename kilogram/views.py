202464c7874fdc85f081d2f525b43f3e943399f6
seulgiwendy <wheejuni@gmail.com>
2018-01-19 00:25:08 +0900

    [view]미 로그인 중 업로드페이지 방문시 로그인으로 리디렉션

--- kilogram/views.py
+++ kilogram/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -8,6 +9,18 @@ from kilogram.forms import CreateUserForm, UploadForm
 
 
 def upload(request):
+    if request.method == "POST":
+        form = UploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.owner = request.user
+            form.save()
+            return redirect('kilogram:index')
+
+    if (isinstance(request.user, User) == False):
+        return redirect('/accounts/login')
+
     form = UploadForm()
     return render(request, 'kilogram/upload.html', {'form' : form})
 
